e00cbfbcd48ebc6ae13b6f3becac9f0aec6c9770
pagidas <pagakrivos@gmail.com>
2019-03-05 00:40:23 +0200

    Demonstrates the basic concept of anonymous classes

---
+++ src/AnonymousClasses/HelloAnonymousClasses.java
@@ -0,0 +1,69 @@
+package AnonymousClasses;
+
+public class HelloAnonymousClasses {
+
+    interface DogBarking {
+        void bark();
+        void bark(String dog);
+        void barkTo(String dog);
+    }
+
+    public void DogsBark() {
+
+        /*
+         *  Traditional way to implement an interface by declaring a new Class
+         *  and creating an instance of that class to call a specific method.
+         */
+        class LabradorRetriever implements DogBarking {
+            @Override
+            public void bark() {
+                System.out.println("Labrador Retriever barks!");
+            }
+
+            @Override
+            public void bark(String dog) {
+                barkTo(dog);
+            }
+
+            @Override
+            public void barkTo(String dog) {
+                System.out.println("Labrador Retriever barks at " + dog + "!");
+            }
+        }
+
+        LabradorRetriever labradorRetriever = new LabradorRetriever();
+
+        /*
+         *  Creates an anonymous class and calls the
+         *  method directly from the specified reference.
+         */
+        DogBarking beagle = new DogBarking() {
+            @Override
+            public void bark() {
+                System.out.println("Beagle barks!");
+            }
+
+            @Override
+            public void bark(String dog) {
+                barkTo(dog);
+            }
+
+            @Override
+            public void barkTo(String dog) {
+                System.out.println("Beagle barks at " + dog + "!");
+            }
+        };
+
+        labradorRetriever.bark();
+        beagle.bark("Labrador");
+        labradorRetriever.bark("Beagle");
+    }
+
+    public static void main(String[] args) {
+
+        HelloAnonymousClasses app = new HelloAnonymousClasses();
+        app.DogsBark();
+
+    }
+
+}
